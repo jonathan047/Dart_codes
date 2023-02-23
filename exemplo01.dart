74bfb20e15c8448e5bbcbeabe3ec5d8ad268f135
jonathan047 <barrosjonathan737@gmail.com>
2023-02-23 00:52:15 -0300

    Create exemplo01.dart
    1º exemplo em Dart.

---
+++ exemplo01.dart
@@ -0,0 +1,8 @@
+void main(){
+  List <String> lista = ['Jonathan','Barros'];
+  List <int> lista1 = [1,2];
+  List <num> lista2 = [2.1,3.3];
+  print('Nome: ${lista[0]}  ID: ${lista1[1]}  número: ${lista2[0]}');
+  print('Nome: ${lista[1]}  ID: ${lista1[0]}  numero: ${lista2[1]}');
+  print('Soma de números: ${lista2[0] + lista2[1]}');
+}
